3da0348aee4d802b134ac15eea8b4a479f67d3cf
Arian27Kapllanaj <ariankapllanaj@outlook.com>
2021-08-22 21:04:18 +0200

    first commit, adding components

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import Topbar from "./components/topbar/Topbar";
+import Intro from "./components/intro/Intro";
+import Portfolio from "./components/portfolio/Portfolio";
+import Works from "./components/works/Works";
+import Experimentals from "./components/experimentals/Experimentals";
+import Contact from "./components/contact/Contact";
+import "./app.scss";
+
+function App() {
+  return (
+    <div className="app">
+      <Topbar/>
+      <div className="sections">
+        <Intro/>
+        <Portfolio/>
+        <Works/>
+        <Experimentals/>
+        <Contact/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/experimentals/Experimentals.jsx
@@ -0,0 +1,9 @@
+import "./experimentals.scss"
+
+export default function Experimentals() {
+    return (
+        <div className="experimentals">
+            
+        </div>
+    )
+}
