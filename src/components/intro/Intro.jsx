3d5068886123666d777ce68b581d920c9bc8e2ad
Arian27Kapllanaj <ariankapllanaj@outlook.com>
2021-08-25 18:56:34 +0200

    building intro page

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,9 +1,25 @@
 import "./intro.scss"
 
 export default function intro() {
-    return (
-        <div className="intro" id="intro">
-
+  return (
+    <div className="intro" id="intro">
+        <div className="left">
+            <div className="imgContainer">
+                <img src="assets/photo.png" alt="me" />
+            </div>
         </div>
-    )
+        <div className="right">
+            <div className="wrapper">
+                <h2>Hi there, I am</h2>
+                <h1>Arian Kapllanaj</h1>
+                <h3><span>How are you?</span></h3>
+                
+            </div>
+            <a href="#portfolio">
+                <img src="assets/down.png" alt="arrow down" />
+            </a>
+        </div>
+
+      </div>
+  );
 }
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -13,7 +13,7 @@ export default function Menu({ menuOpen, setMenuOpen }) {
                 <li onClick={()=>setMenuOpen(false)}>
                     <a href="#works">Works</a>
                 </li>
-                <li>
+                <li onClick={()=>setMenuOpen(false)}>
                     <a href="#experimentals">Experimentals</a>
                 </li>
                 <li onClick={()=>setMenuOpen(false)}>
