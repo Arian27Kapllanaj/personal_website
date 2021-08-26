12ae56762933ad32dbac601da8ab60bca74ed578
Arian27Kapllanaj <ariankapllanaj@outlook.com>
2021-08-26 14:34:30 +0200

    adding a slider in work component

--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -1,9 +1,61 @@
-import "./works.scss"
+import { useState } from "react";
+import "./works.scss";
 
 export default function Works() {
-    return (
-        <div className="works" id="works">
-            
-        </div>
-    )
+    const  [currentSlide, setCurrentSlide] = useState(0);
+  const data = [
+    {
+      id: "1",
+      icon: "assets/twitter.png",
+      title: "Twitter Clone",
+      desc: "Twitter Clone using Laravel 6 and Boostrap 4",
+      img: "assets/twitter.png",
+    },
+    {
+      id: "2",
+      icon: "assets/twitter.png",
+      title: "Twitter Clone2",
+      desc: "Twitter Clone using Laravel 6 and Boostrap 4",
+      img: "assets/twitter.png",
+    },
+    {
+      id: "3",
+      icon: "assets/twitter.png",
+      title: "Twitter Clone3",
+      desc: "Twitter Clone using Laravel 6 and Boostrap 4",
+      img: "assets/twitter.png",
+    },
+  ];
+
+  const handleClick = (way)=> {
+      way === "left" ? setCurrentSlide(currentSlide > 0 ? currentSlide-1 : 2) :
+      setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
+  };
+  return (
+    <div className="works" id="works">
+      <div className="slider" style={{transform:`translateX(-${currentSlide * 100}vw)` }}>
+        {data.map((d) => (
+          <div className="container">
+            <div className="item">
+              <div className="left">
+                <div className="leftContainer">
+                  <div className="imgContainer">
+                    <img src={d.icon} alt="" />
+                  </div>
+                  <h2>{d.title}</h2>
+                  <p>{d.desc}</p>
+                  <span>Projects</span>
+                </div>
+              </div>
+              <div className="right">
+                <img src={d.img} alt="" />
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+      <img src="assets/arrow.png" className="arrow left" alt="" onClick={()=>handleClick("left")}/>
+      <img src="assets/arrow.png" className="arrow right" alt=""  onClick={()=>handleClick("right")}/>
+    </div>
+  );
 }
